862554ee6f43cd71b75512291e5d3c7366e2eca7
Pentti Laitinen <pentti.laitinen@gmail.com>
2024-07-27 13:41:27 +0300

    added main function content

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('xerxes')
+import { argv } from "node:process";
+function main() {
+    if (argv.length < 3) {
+        console.log("ERROR: program needs one argument");
+        return 10;
+    }
+    if (argv.length > 3) {
+        console.log("ERROR: too many arguments");
+        return 11;
+    }
+    const baseURL = argv[2];
+    console.log("Starting web crawling from " + baseURL);
+}
+
+main();
